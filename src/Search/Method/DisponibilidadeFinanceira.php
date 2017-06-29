a5b3873a3baed3c7f1a2d719e2c00659a0b35164
Carlos Rodrigues <carlosrodriguesf96@gmail.com>
2017-06-29 11:33:06 -0300

    Issue #10 - Adicionar busca de disponibilidade financeira

---
+++ src/Search/Method/DisponibilidadeFinanceira.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SonnyBlaine\RoveretiBridge\Search\Method;
+
+use SonnyBlaine\IntegratorBridge\RequestInterface;
+use SonnyBlaine\RoveretiBridge\MethodInterface;
+use Simonetti\Rovereti as SDK;
+
+class DisponibilidadeFinanceira implements MethodInterface
+{
+    private const URI = '/Api/ContaPagar/DisponibilidadeFinanceira/jsonData';
+    private const METHOD_NAME = 'BuscarDisponibilidadeFinanceira';
+
+    public function validateMethod(RequestInterface $request)
+    {
+        return self::METHOD_NAME === $request->getMethodIdentifier();
+    }
+
+    public function execute(SDK\Client $client, RequestInterface $request)
+    {
+        $codEmpresa = intval($request->getData()->codEmpresa);
+        if (!$codEmpresa) {
+            throw new \Exception("Sorry, codEmpresa is empty!");
+        }
+
+        $anoMes = \DateTime::createFromFormat("Ym", $request->getData()->anoMes);
+        if (!$anoMes) {
+            throw new \Exception("Sorry, data is empty os invalid!");
+        }
+
+        $centrosCusto = $request->getData()->centrosCusto;
+        if (is_string($centrosCusto)) {
+            $centrosCusto = json_decode($centrosCusto);
+        }
+        if (empty($centrosCusto)) {
+            $centrosCusto = [];
+        }
+
+        $buscarDisp = new SDK\BuscarDisponibilidadeFinanceira($client);
+        return $buscarDisp->execute(self::URI, $codEmpresa, $anoMes, $centrosCusto);
+    }
+}
